cf0721980451b9ea9a9fb18ca0c9e1e12fcf9207
zhoums <zms825@163.com>
2018-09-07 17:56:50 +0800

    'kkk'

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,9 @@
+
+chrome.browserAction.onClicked.addListener(function(tab){  
+  // var newURL = "your url";
+  // chrome.tabs.create({ url: newURL });
+  main();
+});
 //获取抓取v任务的达人id
 function getAllVTaskDarenIds(page){
 	var _page = page||1;
@@ -20,7 +26,7 @@ function getDarenArticleUrl(page){
 	var _page = page||1;
 	return new Promise(function(resolve,reject){
 		$.ajax({
-			url:'http://spider.ittun.com/spider/tb/front/getDarenArticleUrl.wb?page='+_page+'&pageSize=2',
+			url:'http://spider.ittun.com/spider/tb/front/getDarenArticleUrl.wb?page='+_page+'&pageSize=20',
 			beforeSend: function (XMLHttpRequest) {
 	         		XMLHttpRequest.setRequestHeader("token", "KE923jddu#@(DFDJiw1dI$*FYHHHHH");
 	             },
@@ -31,6 +37,15 @@ function getDarenArticleUrl(page){
 	})
 }
 
+function sleep(numberMillis) {  
+        var now = new Date();    
+        var exitTime = now.getTime() + numberMillis;   
+        while (true) { 
+            now = new Date();       
+            if (now.getTime() > exitTime) 
+            return;    
+        } 
+    }
 
 async function main(){
 	var allArticle=[];
@@ -43,10 +58,49 @@ async function main(){
 			allArticle=allArticle.concat(pageData.result.list);
 		}
 	}
+	console.log('allArticle',allArticle,allArticle[0])
+	return;
 	//对每个URL做处理
 	$.each(allArticle,function(ind,article){
-
+		console.log(article);
+		updateTab(article.url)
+		/*chrome.tabs.getSelected(null, function(tab){
+			 sleep(1000);
+	        console.log(tab,tab.id,tab.url);
+	        var _id=tab.id;
+	        chrome.tabs.update(_id, {
+				'url': article.url,
+				'selected': true
+			},function(result){
+				console.log('update',result)
+			});
+	    });*/
+	   sleep(500);
+		/*chrome.tabs.getCurrent(function(tab){
+			console.log(tab)
+			chrome.tabs.update(tab.id, {
+				'url': 'https://www.baidu.com',
+				'selected': true
+			});
+		});*/
 	})
 	var darenIds = await getAllVTaskDarenIds();
 	console.log('llll',articleUrl,darenIds);
+}
+
+
+function updateTab(url){
+	chrome.tabs.getSelected(null, function(tab){
+        var _id=tab.id;
+        chrome.tabs.update(_id, {
+			'url': url,
+			'selected': true
+		},function(result){
+			console.log('update',result,_id)
+			sleep(1000)
+			chrome.tabs.sendRequest(_id, {greeting: "hello"}, function(response) {
+			    console.log('adjslkf',response);
+			});
+		});
+    });
 }
